97167629505ae8711e530c0e06206a6e0fd6c50b
vadwwxz <vadwwxz@gmail.com>
2023-03-27 20:08:44 +0300

    Created tile assets

--- Assets/Scripts/TileState.cs
+++ Assets/Scripts/TileState.cs
@@ -1,18 +1,8 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
-public class TileState : MonoBehaviour
+[CreateAssetMenu(menuName = "Tile State")]
+public class TileState : ScriptableObject
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    public Color backgroundColor;
+    public Color textColor;
 }
